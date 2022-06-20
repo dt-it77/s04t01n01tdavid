93b4b4fc0008cc77d400c1806e22163fbd6b775b
David <davidt.it77@gmail.com>
2022-06-20 23:44:20 +0200

    first commit

---
+++ src/main/java/s04t01n01tdavid/s04t01n01tdavid/S04t01n01tdavidApplication.java
@@ -0,0 +1,22 @@
+package s04t01n01tdavid.s04t01n01tdavid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04t01n01tdavidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04t01n01tdavidApplication.class, args);
+	}
+/*
+ * 
+ * 
+
+Urls:
+
+Spring Tools 4: Getting Started in Eclipse - YouTube
+
+Spring | Spring Quickstart Guide
+ */
+}
---
+++ src/test/java/s04t01n01tdavid/s04t01n01tdavid/S04t01n01tdavidApplicationTests.java
@@ -0,0 +1,13 @@
+package s04t01n01tdavid.s04t01n01tdavid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04t01n01tdavidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
